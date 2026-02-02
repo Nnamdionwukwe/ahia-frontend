a5de373e584dbfb537e8464446f44f53927b2199
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-02-02 01:29:55 +0100

    fvefeve

--- src/components/DeliveryDetails/DeliveryDetails.jsx
+++ src/components/DeliveryDetails/DeliveryDetails.jsx
@@ -13,23 +13,32 @@ const DeliveryDetails = () => {
       </div>
 
       <div className={styles.deliveryDetails}>
-        <div className={styles.deliveryRow}>
-          <span className={styles.deliveryLabel}>Standard:</span>
-          <span className={styles.deliveryValue}>free on all orders.</span>
-          <button className={styles.clickCollect}>Click & Collect</button>
-        </div>
-        <div className={styles.deliveryRow}>
-          <span className={styles.deliveryLabel}>Delivery:</span>
-          <span className={styles.deliveryValue}>
-            Arrives in NG in as little as 7 days
-          </span>
-        </div>
-        <div className={styles.deliveryRow}>
-          <span className={styles.deliveryLabel}>Courier company:</span>
-          <div className={styles.courierLogos}>
-            <span className={styles.courierBadge}>Speedaf</span>
-            <span className={styles.courierBadge}>GIG</span>
+        <div>
+          <div className={styles.deliveryRow}>
+            <span className={styles.deliveryLabel}>Standard:</span>
+            <span className={styles.deliveryValue}>free on all orders.</span>
           </div>
+          <div className={styles.deliveryRow}>
+            <span className={styles.deliveryLabel}>Delivery:</span>
+            <span className={styles.deliveryValue}>
+              Arrives in NG in as little as 7 days
+            </span>
+          </div>
+          <div className={styles.deliveryRow}>
+            <span className={styles.deliveryLabel}>Courier company:</span>
+            <div className={styles.courierLogos}>
+              <span className={styles.courierBadge}>Speedaf</span>
+              <span className={styles.courierBadge}>GIG</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.deliveryRow2}>
+          <button className={styles.clickCollect}>
+            Click & Collect: FREE.
+          </button>
+
+          <div className={styles.collectInfo}>Delivery: Feb 10-23</div>
         </div>
       </div>
     </>
