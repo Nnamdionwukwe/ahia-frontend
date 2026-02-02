5209ab924c4ada0be53df76ecb0b50079729bc2e
Onwukwennamdi <nnamdionwukwe@gmail.com>
2026-02-02 01:55:02 +0100

    dgdwfgw

--- src/components/ProductSpecs/ProductSpecs.jsx
+++ src/components/ProductSpecs/ProductSpecs.jsx
@@ -4,17 +4,6 @@ import styles from "./ProductSpecs.module.css";
 const ProductSpecs = ({ attributes }) => {
   if (Object.keys(attributes).length === 0) return null;
 
-  const getSpecIcon = (group) => {
-    switch (group) {
-      case "battery":
-        return "🔋";
-      case "waterproof":
-        return "💧";
-      default:
-        return "📱";
-    }
-  };
-
   return (
     <div className={styles.specsGrid}>
       {Object.entries(attributes)
@@ -22,11 +11,10 @@ const ProductSpecs = ({ attributes }) => {
         .map(([group, attrs]) =>
           attrs.slice(0, 1).map((attr, idx) => (
             <div key={`${group}-${idx}`} className={styles.specCard}>
-              <div className={styles.specIcon}>{getSpecIcon(group)}</div>
               <div className={styles.specLabel}>{attr.name}</div>
               <div className={styles.specValue}>{attr.value}</div>
             </div>
-          ))
+          )),
         )}
     </div>
   );
